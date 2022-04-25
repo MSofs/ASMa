0f1c7bf49c5dc9c5589f0fceba4f51f4762b65f1
MSofs <49160671+MSofs@users.noreply.github.com>
2022-04-25 19:30:07 +0100

    Add files via upload

---
+++ src/GetHelp.java
@@ -0,0 +1,74 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
+
+public class GetHelp implements java.io.Serializable{
+
+    private InfoPosition p;
+    private ArrayList<Position> enemy_pos;
+
+    public GetHelp(InfoPosition i,ArrayList<Position> enemy_p ) {
+        super();
+        this.p = i;
+        this.enemy_pos = enemy_p;
+    }
+
+    public double calcularDis(Position p1, Position p2) {
+        return Math.sqrt((p2.getY() - p1.getY()) * (p2.getY() - p1.getY()) + (p2.getX() - p1.getX()) * (p2.getX() - p1.getX()));
+    }
+
+    public ArrayList<Double> getEnemyDis(){
+        ArrayList<Double> lista = new ArrayList<>();
+        for(Position po : enemy_pos){
+            double dist = calcularDis(this.p.getPosition(),po);
+            lista.add(dist);
+        }
+    return lista;
+    }
+
+    public AID getAgent() {
+        return p.getAgent();
+    }
+
+    public void setAgent(AID agent) {
+        this.p.setAgent(agent);
+    }
+
+    public Position getPos() {
+        return p.getPosition();
+    }
+
+    public void setPos(Position init) {
+        this.p.setPosition(init);
+    }
+
+    public ArrayList<Position> getEnemyP() {
+        ArrayList<Position> lista = new ArrayList<>();
+        for (Position p : enemy_pos){
+            lista.add(p);
+        }
+        return lista;
+    }
+
+
+    public int getNrEnemys() {
+        return this.enemy_pos.size();
+    }
+
+    @Override
+    public String toString() {
+        return "HelpRequest [agent=" + getAgent() + ", pos=" + getPos() + ", nr_enemys=" + getNrEnemys() + ", pos_inimigos="+ getEnemyP() +"]";
+    }
+
+
+
+}
---
+++ src/Lider.java
@@ -0,0 +1,71 @@
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
+
+import java.util.ArrayList;
+
+
+public class Lider extends Agent {
+
+    protected void setup() {
+        super.setup();
+
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("lider");
+        sd.setName(getLocalName());
+        dfd.addServices(sd);
+
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
+
+        this.addBehaviour(new Receiver());
+    }
+
+    protected void takeDown() {
+
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
+        super.takeDown();
+
+    }
+
+
+    private class Receiver extends CyclicBehaviour {
+
+        private ArrayList<InfoPosition> player_position = new ArrayList<InfoPosition>();
+
+        public void action() {
+            ACLMessage msg = receive();
+            if (msg != null) {
+
+                if (msg.getPerformative() == ACLMessage.SUBSCRIBE) {
+
+                    try {
+                        System.out.println(myAgent.getAID().getLocalName() + ": " + msg.getSender().getLocalName()
+                                + " registered!");
+
+                        InfoPosition content = (InfoPosition) msg.getContentObject();
+                        player_position.add(content);
+
+                    } catch (UnreadableException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
