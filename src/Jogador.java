38e6f387dec3d932e7004f09ba4c8a447321d0b5
SrGoncalo <49476758+SrGoncalo@users.noreply.github.com>
2022-05-07 16:56:13 +0100

    Update Jogador.java

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,6 +1,7 @@
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
@@ -12,17 +13,22 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Jogador extends Agent{
+public class Jogador extends Agent {
 
     private InfoPosition current_location;
     private ArrayList<Position> enemy_pos;
-    private boolean tipo; //ofensivo = true, senao defensivo
-    // e preciso uma cena para identificar a equipa ???
+    private String equipa;
+    private String tipo;
+
 
     protected void setup() {
         super.setup();
+        Object[] o = getArguments();
+        this.equipa = (String) o[1];
+        this.tipo = (String) o[2];
 
         this.addBehaviour(new Register_Player());
+        this.addBehaviour(new Request(this,10000));
         //this.addBehaviour(new Receiver());
     }
 
@@ -35,7 +41,8 @@ public class Jogador extends Agent{
 
             DFAgentDescription template = new DFAgentDescription();
             ServiceDescription sd = new ServiceDescription();
-            sd.setType("lider");
+            if (equipa.equals("EquipaA")) sd.setType("EquipaA");
+            else sd.setType("EquipaB");
             template.addServices(sd);
 
             try {
@@ -43,18 +50,14 @@ public class Jogador extends Agent{
 
                 // If Lider is available!
                 if (result.length > 0) {
-
-                    Random rand = new Random();
-                    Position p = new Position(rand.nextInt(35), rand.nextInt(35));
-                    //talvez ter cuidados nao ter jogadores em cima uns dos outros.
-
-                    current_location = new InfoPosition(myAgent.getAID(), p);
+                    Object[] p = getArguments();
+                    current_location = new InfoPosition(myAgent.getAID(), (Position) p[0]);
 
                     ACLMessage msg = new ACLMessage(ACLMessage.SUBSCRIBE);
                     msg.setContentObject(current_location);
 
-                    for (int i = 0; i < result.length; ++i) {
-                        msg.addReceiver(result[i].getName());
+                    for (DFAgentDescription dfAgentDescription : result) {
+                        msg.addReceiver(dfAgentDescription.getName());
                     }
 
                     myAgent.send(msg);
@@ -71,4 +74,87 @@ public class Jogador extends Agent{
         }
     }
 
-}
+    private class Request extends TickerBehaviour { // mandar update posicoes e pedido de ajuda
+
+        public Request(Agent a, long period) {
+            super(a, period);
+        }
+
+        protected void onTick() {
+
+            DFAgentDescription template = new DFAgentDescription();
+            ServiceDescription sd = new ServiceDescription();
+            if (equipa.equals("EquipaA")) sd.setType("EquipaA");
+            else sd.setType("EquipaB");
+            template.addServices(sd);
+
+            try {
+                DFAgentDescription[] result = DFService.search(myAgent, template);
+
+                if (result.length > 0) {
+                    ACLMessage msg = receive(); //se nao houver nenhuma posicao para ir, entao vai para o centro 17*17
+                    if (msg == null) {
+
+                        Position nova = current_location.getPosition();
+                     // mais a frente vai ser preciso ver se nao existe ninguem no quadrado onde vai tentar ir.int
+
+                       int x = nova.getX();
+                       int y = nova.getY();
+
+                        if (x<17) nova.setX(x+1);
+                        else if (x>17) nova.setX(x-1);
+                        else if (y<17) nova.setY(y+1);
+                        else if (y>17) nova.setY(y-1);
+
+
+                        //System.out.println("Antiga Posição: "+x + ","+ y+" NOVA POSIÇÃO: "+ nova);
+
+                        ACLMessage mensagem = new ACLMessage(ACLMessage.INFORM);
+                        mensagem.setContentObject(current_location);
+
+                        for (DFAgentDescription dfAgentDescription : result) {
+                            mensagem.addReceiver(dfAgentDescription.getName());
+                        }
+                        myAgent.send(mensagem);
+                    }else{
+                        // e porque ele recebeu mensagem para onde se dirigir
+
+                        Position p = (Position) msg.getContentObject();
+
+                        int x = current_location.getPosition().getX();
+                        int y = current_location.getPosition().getY();
+
+
+                        if(x<p.getX())current_location.setPositionX(x+1);
+                        else if(x>p.getX())current_location.setPositionX(x-1);
+                        else if(y<p.getY())current_location.setPositionX(y+1);
+                        else if(y>p.getY())current_location.setPositionX(y-1);
+
+
+                        ACLMessage mensagem = new ACLMessage(ACLMessage.INFORM);
+                        mensagem.setContentObject(current_location);
+
+                        for (DFAgentDescription dfAgentDescription : result) {
+                            mensagem.addReceiver(dfAgentDescription.getName());
+                        }
+                        myAgent.send(mensagem);
+                    }
+
+                    } else {
+                     System.out.println(myAgent.getAID().getLocalName() + ": No Lider available. Agent offline");
+                    }
+
+                    //if() // como ver se tem inimigo dentro do campo visao ?
+                // por aqui o behaviour para pedir ajuda
+
+                } catch (IOException | FIPAException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (UnreadableException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    }
